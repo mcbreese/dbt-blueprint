c6d8e2b0000444a9aac4c49cbd57ad979e7aba32
Alex Teodosiu <mihail.alexandru.teodosiu@vippsmobilepay.com>
2025-09-12 16:40:47 +0200

    Add ephemeral materialization to age group validation test

--- dbt_blueprint/unit_tests/macros/calculate_age_group/validate__calculate_age_group.sql
+++ dbt_blueprint/unit_tests/macros/calculate_age_group/validate__calculate_age_group.sql
@@ -1,3 +1,7 @@
+{# Make this model ephemeral so it doesnt create a table in the database #}
+
+{{ config(materialized='ephemeral') }}
+
 with dummy_source as (
     select * from {{ ref('test_source')}}
 )
