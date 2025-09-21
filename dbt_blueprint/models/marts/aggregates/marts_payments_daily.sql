0304d4b56ff64aa9f9a5564efde023cb8dab4911
Alex Teodosiu <mihail.alexandru.teodosiu@vippsmobilepay.com>
2025-09-21 22:14:26 +0200

    Remove incremental materialization configuration from marts_payments_daily model

--- dbt_blueprint/models/marts/aggregates/marts_payments_daily.sql
+++ dbt_blueprint/models/marts/aggregates/marts_payments_daily.sql
@@ -1,10 +1,3 @@
-{{ config(
-  materialized='incremental',
-  incremental_strategy='merge',
-  unique_key=['merchant_id','event_date'],
-  incremental_predicates=["event_date >= date_sub(current_date, {{ var('days_back', 14) }})"]
-) }}
-
 with tx as (
   select
       merchant_id,
@@ -13,9 +6,6 @@ with tx as (
       event_state
   from {{ ref('fact_transaction') }}
   where event_state = 'captured'
-  {% if is_incremental() %}
-    where date(event_ts) >= date_sub(current_date, {{ var('days_back', 14) }})
-  {% endif %}
 )
 
 select
